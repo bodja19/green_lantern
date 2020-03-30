afdc7c82aae9d19acc98f289a42ccbc27ebc0221
bodja19 <bodja19@gmail.com>
2020-03-30 15:02:37 +0300

    robot,not ready

--- lantern/tdd/robot.py
+++ lantern/tdd/robot.py
@@ -1,7 +1,7 @@
 class Asteroid:
-    def __init__(self, x, y):
-        self.x = x
-        self.y = y
+    def __init__(self, width, height):
+        self.width = width
+        self.height = height
 
 
 class Robot:
@@ -10,22 +10,108 @@ class Robot:
         self.y = y
         self.direction = direction
         self.asteroid = asteroid
-        if self.x > self.asteroid.x:
+        if self.x > self.asteroid.width:
             raise MissAsteroidErro()
-        if self.y > self.asteroid.y:
+        if self.y > self.asteroid.height:
             raise MissAsteroidErro()
 
     def turn_left(self):
         turns = {"E": "N",
                  "N": "W",
                  "W": "S",
-                 "S": "E"
-         }
+                 "S": "E",
+                 }
+
         self.direction = turns[self.direction]
 
-    """def turn_right(self):
-        turns = {"E": "S"}
-        self.direction = turns[self.direction]"""
+    def turn_right(self):
+        turns = {"N": "E",
+                 "E": "S",
+                 "S": "W",
+                 "W": "N",
+                 }
+        self.direction = turns[self.direction]
+
+    def move_forwa(self):
+
+        if self.direction == "E":
+            self.x += 1
+        elif self.direction == "N":
+            self.y += 1
+        elif self.direction == "S":
+            self.y -= 1
+        elif self.direction == "W":
+            self.x -= 1
+        else:
+            raise MoveError("Choose direction")
+        self.check_drop()
+
+    def move_back(self):
+
+        if self.direction == "E":
+            self.x -= 1
+        elif self.direction == "N":
+            self.y -= 1
+        elif self.direction == "S":
+            self.y += 1
+        elif self.direction == "W":
+            self.x += 1
+        else:
+            raise MoveError("Вибери правильний напрямок")
+        self.check_drop()
+        self.check_obstacle(0, 0)
+
+    def check_drop(self):
+        if self.x > self.asteroid.width:
+            raise MoveError("Drop from asteroid")
+        if self.y > self.asteroid.height:
+            raise MoveError("Drop from asteroid")
+
+    def check_obstacle(self, obstacle_x, obstacle_y):
+
+        if obstacle_x >= self.asteroid.width:
+            raise MissAsteroidErro()
+        if obstacle_y >= self.asteroid.height:
+            raise MissAsteroidErro()
+        if self.x == obstacle_x:
+            raise MoveError("You have problem with obstacle")
+        if self.y == obstacle_y:
+            raise MoveError("You have problem with obstacle")
+
 
 class MissAsteroidErro(Exception):
     pass
+
+
+class MoveError(Exception):
+    pass
+
+
+""" if self.direction == "E":
+            self.x += 1
+            return self.x
+        elif self.direction == "N":
+            self.y += 1
+        elif self.direction == "S":
+            self.y -= 1
+        elif self.direction == "W":
+            self.x -= 1
+        return self.x, self.y
+
+        forward = {"N": (0, 1),
+                   "E": (1, 0),
+                   "S": (0, 1),
+                   "W": (1, 0)
+                   }
+
+        self.direction = forward[self.direction]
+        self.x += forward[self.x]
+
+
+                try:
+            self.x, self.y < self.width, self.height
+            )
+            raise MoveError
+        except:
+            pass
+        """
--- lantern/tdd/test.py
+++ lantern/tdd/test.py
@@ -1,12 +1,31 @@
 import pytest
-from robot import Robot, Asteroid, MissAsteroidErro
+from robot import Robot, Asteroid, MissAsteroidErro, MoveError
+
+"""Порядок денний:
+Створіть робота з положенням і напрямком
+Перевірте, чи робот пропустив астероїд під час посадки
+Створіть і випробуйте функції turn_left і turn_right
+Додайте функції move_forward, move_backward
+Перевірте, чи падає він від астероїда під час руху
+Додайте астероїдні перешкоди
+Оновіть рух робота, щоб дотримуватися перешкод
+... Ваші ідеї
+
+Домашня робота
+Закінчіть тестування оборотів
+Логіка руху коду move_forward, move_backward
+Перевірте, чи не падає робот з астероїду
+Для сміливих людей
+Додайте перешкоди астероїду
+Створіть роботу почесні перешкоди
+"""
 
 
 class TestRobotCreation:
     def test_parameters(self):
         x, y = 10, 15
         direction = "E"
-        asteroid = Asteroid(x + 1, y + 1)
+        asteroid = Asteroid(30, y + 1)
         robot = Robot(x, y, asteroid, direction)
         assert robot.direction == direction
         assert robot.x == 10
@@ -21,7 +40,6 @@ class TestRobotCreation:
                 ((15, 25), (15, 27)),
         )
     )
-
     def test_check_if_robot_on_asteroid(self, asteroid_size, robot_coordinates):
         with pytest.raises(MissAsteroidErro):
             asteroid = Asteroid(*asteroid_size)
@@ -32,31 +50,108 @@ class TestMove:
     def setup(self):
         self.x, self.y = 10, 15
         self.asteroid = Asteroid(self.x + 1, self.y + 1)
-        self.direction = self.curent_direction
+
     @pytest.mark.parametrize(
         "curent_direction,expected_direction",
         (
-        ("N", "W"),
-        ("W", "S"),
-        ("S", "E"),
+                ("N", "W"),
+                ("W", "S"),
+                ("S", "E"),
         )
     )
-
     def test_turn_left(self, curent_direction, expected_direction):
-        print(self.setup())
-        print(self.asteroid)
-        print(curent_direction)
-        robot = Robot(self.setup, self.asteroid, curent_direction)
+        robot = Robot(self.x, self.y, self.asteroid, curent_direction)
         robot.turn_left()
         assert robot.direction == expected_direction
 
+    @pytest.mark.parametrize(
+        "curent_direction,expected_direction",
+        (
+                ("N", "E"),
+                ("W", "N"),
+                ("S", "W"),
+        )
+    )
+    def test_turn_right(self, curent_direction, expected_direction):
+        robot = Robot(self.x, self.y, self.asteroid, curent_direction)
+        robot.turn_right()
+        assert robot.direction == expected_direction
 
-    """def test_turn_right(self):
-        x, y = 10, 15
+    @pytest.mark.parametrize(
+        "move_forward,expected_move,move_direction",
+        [(15, 14, "S"),
+         (10, 11, "E"),
+         (10, 9, "W"),
+         ]
+    )
+    def test_move_forward(self, move_forward, move_direction, expected_move):
+        robot = Robot(self.x, self.y, self.asteroid, move_direction)
+        robot.move_forwa()
+        assert robot.x, robot.y == expected_move
+
+    @pytest.mark.parametrize(
+        "move_back,expected_move,move_direction",
+        [(15, 16, "S"),
+         (10, 9, "E"),
+         (30, 30, "W"),
+         ]
+    )
+    def test_move_backward(self, move_back, move_direction, expected_move):
+        robot = Robot(self.x, self.y, self.asteroid, move_direction)
+        robot.move_back()
+        print(move_back, expected_move)
+        print(robot.x, robot.y)
+        assert robot.x, robot.y == expected_move
+
+    """(15, "MoveError", "B")- 'вставив в тюпл' перевіряв чи давши неправильний напрямок вилітає помилка- все 
+    праціє тільки як додати в тест незнаю"""
+
+    def test_drop_robot_asteroid(self):
+        asteroid = Asteroid(14, 25)
+        robot = Robot(10, 20, asteroid, "W")
+        robot.check_drop()
+        assert robot.x == 10
+
+    @pytest.mark.parametrize(
+        "obstacle_X,obstacle_Y",
+        (
+                (1, 2),
+                (15, 2),
+                (30, 30),
+        )
+    )
+    def test_obstacle_robot_on_asteroid(self, obstacle_X, obstacle_Y):
+        asteroid = Asteroid(20, 25)
+        robot = Robot(15, 20, asteroid, "W")
+        robot.check_obstacle(obstacle_X, obstacle_Y)
+
+        assert robot.x == 15
+        assert MoveError
+
+        """
+        (self, asteroid_size, robot_coordinates):
+        with pytest.raises(MissAsteroidErro):
+            asteroid = Asteroid(*asteroid_size)
+            Robot(*robot_coordinates, asteroid, "W")
+    @pytest.mark.parametrize(
+        "expected_move,move_direction",
+        [(15, 16, "S"),
+         (10, 9, "E"),
+         (10, 11, "W"),
+         ]
+    )
+         self.asteroid = Asteroid(20, 35)
+        robot = Robot(self.x, self.y, self.asteroid, move_direction)
+        robot.check_drop()
+        assert robot.check_drop == expected_move
+
+
+        x, y = 10, 15 
         direction = "E"
         asteroid = Asteroid(x + 1, y + 1)
         robot = Robot(x, y, asteroid, direction)
-        robot.turn_left()
-        assert robot.direction == "S"""""
-
-
+        assert robot.direction == direction
+        assert robot.x == 10
+        assert robot.y == 15
+        assert robot.asteroid == asteroid
+        assert robot."""

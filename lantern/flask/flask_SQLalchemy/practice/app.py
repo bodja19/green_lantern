5e16b2584948116b619dc2e44a4c6f946a01faa3
bodja19 <bodja19@gmail.com>
2020-05-01 01:42:30 +0300

    add store get to database

--- lantern/flask/flask_SQLalchemy/practice/app.py
+++ lantern/flask/flask_SQLalchemy/practice/app.py
@@ -3,7 +3,7 @@ from flask_sqlalchemy import SQLAlchemy
 from sqlalchemy_utils import create_database, drop_database, database_exists
 # import os
 from Config import Config
-from populate_data import get_users, get_goods
+from populate_data import get_users, get_goods, get_stores
 
 db = SQLAlchemy()
 
@@ -22,6 +22,16 @@ class Product(db.Model):
     brand = db.Column(db.String(80), nullable=False)
     name = db.Column(db.String(200), nullable=False)
     price = db.Column(db.Integer, nullable=False)
+
+class Store(db.Model):
+    __tablename__ = "stores"
+
+    store_id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    name = db.Column(db.String(80), nullable=False)
+    city = db.Column(db.String(200), nullable=False)
+    address = db.Column(db.String(200), nullable=False)
+    manager_id = db.Column(db.Integer, db.ForeignKey('users.user_id'), nullable=False, primary_key=True)
+
 
 
 
@@ -53,6 +63,12 @@ with app.app_context():
     db.session.commit()
     print('Database goods writen base successfully')
 
+with app.app_context():
+    stores = get_stores()
+    for store in stores:
+        db.session.add(Store(**store))
+    db.session.commit()
+    print('Database store writen base successfully')
 
 
 
@@ -65,50 +81,4 @@ with app.app_context():
 
 
 
-
-# from flask import Flask
-# from flask_sqlalchemy import SQLAlchemy
-# from sqlalchemy_utils import create_database, drop_database, database_exists
-#
-# from config import Config
-# from populate_data import get_users
-#
-#
-# db = SQLAlchemy()
-#
-#
-# class User(db.Model):
-#     __tablename__ = "users"
-#
-#     user_id = db.Column(db.Integer, primary_key=True)
-#     name = db.Column(db.String(), nullable=False)
-#     email = db.Column(db.String(), nullable=False)
-#     password = db.Column(db.String(), nullable=False)
-
-
-
-# app = Flask(__name__)
-# app.config.from_object(Config)
-# db.init_app(app)
-#
-# with app.app_context():
-#     if database_exists(db.engine.url):
-#         db.create_all()
-#         print('Database exists')
-#     else:
-#
-#         print(f'Database does not exists{db.engine.url}')
-#         create_database(db.engine,url)
-#         print('Data base created')
-#
-#         print(f"Database does not exists {db.engine.url}")
-#         create_database(db.engine.url)
-#         print('Data base created')
-#
-# with app.app_context():
-#     users = get_users()
-#     for user in users:
-#         db.session.add(User(**user))
-#     db.session.commit()
-#     print('Data written in data_base succesfuly')
 #
--- lantern/flask/flask_SQLalchemy/practice/populate_data.py
+++ lantern/flask/flask_SQLalchemy/practice/populate_data.py
@@ -13,3 +13,9 @@ def get_goods():
         reader = csv.DictReader(f)
         goods = [i for i in reader]
     return goods
+
+def get_stores():
+    with open('stores.csv', 'r') as f:
+        reader = csv.DictReader(f)
+        stores = [i for i in reader]
+    return stores

86a45487a6c789ba00e657361395eb21b556845f
bodja19 <bodja19@gmail.com>
2020-05-21 15:09:12 +0300

    make: logout, hide view info when user auth and not auth add orders

--- lantern/flask/flask_login_and_templates/practice/grocery_store/routes/main.py
+++ lantern/flask/flask_login_and_templates/practice/grocery_store/routes/main.py
@@ -1,6 +1,6 @@
 from flask import Blueprint, render_template
 from grocery_store.database import db
-from flask_login import current_user
+from flask_login import current_user, login_required
 from grocery_store.models import Good
 
 main = Blueprint('main', __name__)
@@ -11,10 +11,16 @@ def index():
     return render_template('index.html')
 
 @main.route('/profile')
+@login_required
 def profile():
+
     return render_template('profile.html', user=current_user.name, email=current_user.email)
 
 @main.route('//goods_page')
 def goods_page():
     rows = Good.query.all()
     return render_template('goods-page.html', rows=rows)
+
+@main.route('//orders')
+def order_page():
+    pass
